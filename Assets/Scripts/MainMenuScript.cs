45035e8816b1db51724e1e74590fbc87499e8962
George McEwen <gm21160@my.acm.ac.uk>
2023-11-03 14:31:37 +0000

    Menu Code

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MainMenuScript : MonoBehaviour
 {
-  
+  public void PlayGame()
+    {
+       
+    }
 }
