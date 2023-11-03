9f11bd419f44ebcb18080bbb422097dc115d534d
George McEwen <gm21160@my.acm.ac.uk>
2023-11-03 15:37:09 +0000

    Further Menu

---
+++ Assets/Scripts/ButtonDisable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDisable : MonoBehaviour
+{
+    public GameObject postStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+   
+}
--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-  public void PlayGame()
+    public GameObject startButton;
+    public void PlayGame()
     {
-       
+        SceneManager.LoadScene("Testing");
     }
+  public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+  public void StartMenu(GameObject objToEnable)
+    {
+        objToEnable.SetActive(true);
+        Destroy(startButton);
+    }
+
+
+
 }
